54c192f96150f86414d097a2c0f9e97fa113584f
kkappolo3-blip <kkappolo3@gmail.com>
2026-03-26 08:05:16 +0800

    Update vitest.config.ts

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
+  base: '/photo-match-tool/', // <--- TAMBAHKAN BARIS INI
   plugins: [react()],
   test: {
     environment: "jsdom",
